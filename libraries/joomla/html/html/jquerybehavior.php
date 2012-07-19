4a1240e90d46d32297cbcd35684c2bf889ec47ba
Nguyen Van Hiep <nvhiep.nt@gmail.com>
2012-07-19 18:02:57 +0700

    Make bootstrap tooltip be used like mootools behavior tooltip (for articles list)

---
+++ libraries/joomla/html/html/jquerybehavior.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  HTML
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Utility class for jquery behaviors
+ *
+ * @package     Joomla.Platform
+ * @subpackage  HTML 
+ */
+abstract class JHtmlJquerybehavior
+{
+	protected static $loaded = array();
+	
+	/**
+	 * Add tooltip for element
+	 * Use element's Title as tooltip content
+	 * 
+	 *	
+	 * @param 	string 	$selector selector for the tooltip
+	 * @param	array 	$params An array of options for the tooltip.
+	 * 					Options for the tooltip can be:
+	 * 						animation 	boolean 			true 	apply a css fade transition to the tooltip
+	 *						placement 	string|function 	'top' 	how to position the tooltip - top | bottom | left | right
+	 *						selector 	string 				false 	If a selector is provided, tooltip objects will be delegated to the specified targets.
+	 *						title 		string | function 	'' 		default title value if `title` tag isn't present
+	 *						trigger 	string 	'hover' 	how 	tooltip is triggered - hover | focus | manual
+	 *						delay 		number | object 	0 		delay showing and hiding the tooltip (ms) - does not apply to manual trigger type
+	 *																If a number is supplied, delay is applied to both hide/show
+	 *																Object structure is: delay: { show: 500, hide: 100 }	
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public static function tooltip($selector = '[rel=tooltip]', $params = array())
+	{
+		// Only load once
+		if (isset(self::$loaded[__METHOD__]))
+		{
+			return;
+		}	
+		
+		$opt['animation'] 	= isset($params['animation']) ? $params['animation'] : true;
+		$opt['placement'] 	= isset($params['placement']) ? $params['placement'] : 'top';
+		$opt['selector'] 	= isset($params['selector']) ? $params['selector'] : false;
+		$opt['title'] 		= isset($params['title']) ? $params['title'] : '';
+		$opt['trigger'] 	= isset($params['trigger']) ? $params['trigger'] : 'hover';
+		$opt['delay'] 		= isset($params['delay']) ? $params['delay'] : 0;
+		
+		$options = json_encode($opt);
+		
+		JFactory::getDocument()->addScriptDeclaration(
+			"
+			(function($){
+				$(document).ready(function (){
+					$('" . $selector . "').tooltip(". $options .")
+				});
+								
+			})(jQuery);
+			"
+		);	
+		
+		self::$loaded[__METHOD__] = true;
+		
+		return;
+	}
+	
+	public static function popover($selector = '[rel=popover]', $params = array())
+	{
+		// Only load once
+		if (isset(self::$loaded[__METHOD__]))
+		{
+			return;
+		}	
+		
+		$opt['animation'] 	= isset($params['animation']) ? $params['animation'] : true;
+		$opt['placement'] 	= isset($params['placement']) ? $params['placement'] : 'top';
+		$opt['selector'] 	= isset($params['selector']) ? $params['selector'] : false;
+		$opt['title'] 		= isset($params['title']) ? $params['title'] : '';
+		$opt['trigger'] 	= isset($params['trigger']) ? $params['trigger'] : 'hover';
+		$opt['delay'] 		= isset($params['delay']) ? $params['delay'] : 0;
+		
+		$options = json_encode($opt);
+		
+		JFactory::getDocument()->addScriptDeclaration(
+			"
+			(function($){
+				$(document).ready(function (){
+					$('" . $selector . "').tooltip(". $options .")
+				});
+								
+			})(jQuery);
+			"
+		);	
+		
+		self::$loaded[__METHOD__] = true;
+		
+		return;
+	}
+}
